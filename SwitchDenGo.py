ef2e207dbd53ffde5d0641bbd6410841f6dd2a7c
mipsparc <mipsparc@gmail.com>
2022-10-03 18:55:53 +0900

    修正

--- SwitchDenGo.py
+++ SwitchDenGo.py
@@ -1,89 +1,73 @@
 import pygame
 import time
 
-class SwitchDenGo(Joystick):
+class SwitchDenGo():
     ACCEL_KNOTCH_NUM = 5
     BRAKE_KNOTCH_NUM = 9
     
+    def __init__(self):
+        pygame.init()
+        self.joy = pygame.joystick.Joystick(0)
+        self.joy.init()
+
     def loadStatus(self):
+        self.brake_knotch = 0
+        self.accel_knotch = 0
         self.buttons = []
         pygame.event.get()
         
-        # 前後左右ボタン
-        hat_x, hat_y = self.joy.get_hat(0)
-        
         # Xボタン
-        if self.joy.get_button(3):
+        if self.joy.get_button(2):
             self.buttons.append("SW_X")
         # Yボタン
-        if self.joy.get_button(0):
+        if self.joy.get_button(3):
             self.buttons.append("SW_Y")
         # Aボタン
-        if self.joy.get_button(2):
+        if self.joy.get_button(0):
             self.buttons.append("SW_A")
         # Bボタン
         if self.joy.get_button(1):
             self.buttons.append("SW_B")
-        # ホームボタン
-        if self.joy.get_button(12):
-            self.buttons.append("SW_HOME")
         # ○ボタン
-        if self.joy.get_button(13):
+        if self.joy.get_button(15):
             self.buttons.append("SW_CIRCLE")
-        # Lボタン
-        if self.joy.get_button(4):
-            self.buttons.append("SW_L")
-        # ZLボタン
-        if self.joy.get_button(6):
-            self.buttons.append("SW_ZL")
-        # Rボタン
+        # HOMEボタン
         if self.joy.get_button(5):
-            self.buttons.append("SW_R")
-        # ZRボタン
-        if self.joy.get_button(7):
-            self.buttons.append("SW_ZR")
-        # +ボタン
-        if self.joy.get_button(9):
-            self.buttons.append("SW_PLUS")
-        # -ボタン
-        if self.joy.get_button(8):
-            self.buttons.append("SW_MINUS")
-        
-        # B9
-        if self.joy.get_button(6):
-            self.brake_knotch = 9
+            self.buttons.append("SW_HOME")
         
         knotch_level = self.joy.get_axis(1)
         
         if knotch_level > 0.95:
             self.accel_knotch = 5
-        elif knotch_level > 0.85:
+        elif knotch_level > 0.75:
             self.accel_knotch = 4
-        elif knotch_level > 0.5:
+        elif knotch_level > 0.55:
             self.accel_knotch = 3
         elif knotch_level > 0.3:
             self.accel_knotch = 2
-        elif knotch_level > 0:
+        elif knotch_level > 0.1:
             self.accel_knotch = 1
         elif knotch_level > -0.05:
             self.accel_knotch = 0
             self.brake_knotch = 0
-        elif knotch_level > -0.2:
+        elif knotch_level > -0.25:
             self.brake_knotch = 1
-        elif knotch_level > -0.3:
+        elif knotch_level > -0.35:
             self.brake_knotch = 2
         elif knotch_level > -0.45:
             self.brake_knotch = 3
-        elif knotch_level > -0.6:
+        elif knotch_level > -0.55:
             self.brake_knotch = 4
-        elif knotch_level > -0.75:
+        elif knotch_level > -0.7:
             self.brake_knotch = 5
-        elif knotch_level > -0.85:
+        elif knotch_level > -0.8:
             self.brake_knotch = 6
-        elif knotch_level > -0.97:
+        elif knotch_level > -0.9:
             self.brake_knotch = 7
-        elif knotch_level >= -1.0:
+        elif knotch_level > -1.0:
             self.brake_knotch = 8
+        else:
+            self.brake_knotch = 9
             
         if self.brake_knotch > 0:
             self.accel_knotch = 0
--- main.py
+++ main.py
@@ -13,49 +13,47 @@ print("随時アップデート(PS2 2ハン対応など)も予定しています
 print()
 print("JRESim_Dengo Version0.1")
 print("使い方: プレイ画面まで移動した後、このプログラムを起動するだけです。")
-print("↑ボタン:前位置、↓ボタン:後位置、Aボタン:EBリセット、警笛1段:HOME、警笛2段:○")
+print("Xボタン:前位置、Bボタン:後位置、Aボタン:EBリセット、警笛1段:HOME、警笛2段:○")
 print("終了時はこのウィンドウをそのまま閉じてください。")
 
-keyboard.press_and_release("S")
+time.sleep(0.1)
+keyboard.write("s", delay=0.05)
 
-dengo = SwitchDenGo()
+dengo = SwitchDenGo.SwitchDenGo()
 P = 0
 B = 0
 while True:
     dengo.loadStatus()
+    
+    plus_knotch = 0
     if P > dengo.accel_knotch:
         plus_knotch = P - dengo.accel_knotch
     if B < dengo.brake_knotch:
         plus_knotch = dengo.brake_knotch - B
     for i in range(plus_knotch):
-        keyboard.press_and_release("Z")
-        time.sleep(0.05)
+        keyboard.write("z", delay=0.05)
     
-    time.sleep(0.05)
-    
+    minus_knotch = 0
     if P < dengo.accel_knotch:
         minus_knotch = dengo.accel_knotch - P
     if B > dengo.brake_knotch:
         minus_knotch = B - dengo.brake_knotch
     if dengo.brake_knotch == 9:
-        keyboard.press_and_release("1")
+        keyboard.write("1", delay=0.05)
     else:
         for i in range(minus_knotch):
-            keyboard.press_and_release("Q")
-            time.sleep(0.05)
+            keyboard.write("q", delay=0.05)
     
     P = dengo.accel_knotch
     B = dengo.brake_knotch
 
-    if "SW_PLUS" in dengo.buttons:
-        keyboard.press_and_release("up arrow")
-    elif "SW_MINUS" in dengo.buttons:
-        keyboard.press_and_release("down arrow")
-    elif "SW_A" in dengo.buttons:
-        keyboard.press_and_release("E")
-    elif "SW_HOME" in dengo.buttons:
-        keyboard.press_and_release("enter")
-    elif "SW_CIRCLE" in dengo.buttons:
-        keyboard.press_and_release("backspace")
-
-    time.sleep(0.01)
+    if "SW_X" in dengo.buttons:
+        keyboard.send("up")
+    if "SW_B" in dengo.buttons:
+        keyboard.send("down")
+    if "SW_A" in dengo.buttons:
+        keyboard.write("e", delay=0.05)
+    if "SW_HOME" in dengo.buttons:
+        keyboard.send("enter")
+    if "SW_CIRCLE" in dengo.buttons:
+        keyboard.send("backspace")
